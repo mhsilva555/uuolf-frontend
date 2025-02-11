cd27d00224095034bc3d6344e54c8e927a4901b5
Marcos Henrique <mhsilva555@gmail.com>
2025-02-11 00:08:51 -0300

    Save project

---
+++ service/tecnologias.js
@@ -0,0 +1,60 @@
+const tecnologias = {
+    linguagens: {
+        imperativas: ['C', 'C++', 'Java', 'Python', 'Ruby', 'Go', 'Rust'],
+        funcionais: ['Haskell', 'Elixir', 'Lisp', 'F#'],
+        orientadas_a_objetos: ['Java', 'C#', 'Ruby', 'Python'],
+        scripts: ['JavaScript', 'PHP', 'Perl', 'Bash'],
+        declarativas: ['SQL', 'HTML', 'CSS'],
+        tipos_de_linguagens: ['Estaticamente tipada', 'Dinamicamente tipada', 'Tipada com inferência', 'Fortemente tipada', 'Fracamente tipada']
+    },
+    frameworks: {
+        frontend: ['React', 'Vue.js', 'Angular', 'Svelte', 'Ember.js', 'Backbone.js'],
+        backend: ['Express.js', 'Django', 'Ruby on Rails', 'Spring', 'Flask', 'ASP.NET'],
+        fullStack: ['Next.js', 'Nuxt.js', 'Meteor', 'Laravel'],
+        mobile: ['React Native', 'Flutter', 'Ionic', 'Xamarin'],
+        desenv_mvc: ['Laravel', 'Spring', 'Ruby on Rails']
+    },
+    bibliotecas: {
+        frontend: ['jQuery', 'Lodash', 'Axios', 'Three.js'],
+        visualizacao: ['D3.js', 'Chart.js', 'Plotly'],
+        testes: ['Jest', 'Mocha', 'Chai', 'Cypress', 'Enzyme'],
+        manipulaDados: ['Moment.js', 'Date-fns'],
+        animações: ['GSAP', 'Anime.js']
+    },
+    ferramentas: {
+        controle_de_versao: ['Git', 'GitHub', 'GitLab', 'Bitbucket'],
+        construção_de_projetos: ['Webpack', 'Parcel', 'Vite', 'Rollup'],
+        automação: ['Gulp', 'Grunt', 'npm scripts'],
+        ambientes_de_desenvolvimento: ['VS Code', 'Sublime Text', 'Atom', 'Eclipse'],
+        CI_CD: ['Jenkins', 'Travis CI', 'CircleCI', 'GitLab CI']
+    },
+    bancos_de_dados: {
+        relacionais: ['MySQL', 'PostgreSQL', 'SQLite', 'MariaDB'],
+        NoSQL: ['MongoDB', 'Cassandra', 'CouchDB', 'Redis', 'Firebase'],
+        grafos: ['Neo4j', 'Amazon Neptune']
+    },
+    tecnologias_web: {
+        protocolos: ['HTTP', 'HTTPS', 'WebSocket', 'FTP', 'SMTP'],
+        APIs: ['REST', 'GraphQL', 'SOAP', 'gRPC'],
+        segurança: ['OAuth', 'JWT', 'CORS', 'SSL/TLS', 'SAML'],
+        front_end: ['HTML', 'CSS', 'JavaScript', 'WebAssembly'],
+        web_services: ['AWS', 'Azure', 'Google Cloud']
+    },
+    plataformas: {
+        sistemas_operacionais: ['Linux', 'Windows', 'macOS'],
+        virtualizacao: ['Docker', 'Kubernetes'],
+        servidores_web: ['Apache', 'NGINX', 'Tomcat', 'IIS']
+    },
+    paradigmas_de_programacao: ['Procedural', 'Orientado a objetos', 'Funcional', 'Reativo', 'Declarativo'],
+    metodologias_de_desenv: ['Ágil', 'Scrum', 'Kanban', 'DevOps', 'TDD'],
+    outras_areas: {
+        inteligencia_artificial: ['Machine Learning', 'Deep Learning', 'Redes Neurais', 'TensorFlow', 'PyTorch'],
+        blockchain: ['Ethereum', 'Solidity', 'Hyperledger'],
+        big_data: ['Hadoop', 'Spark', 'Kafka'],
+        computacao_em_nuvem: ['AWS', 'Azure', 'Google Cloud', 'IBM Cloud'],
+        automacao: ['Selenium', 'Puppeteer', 'Appium'],
+        ferramentas_de_ia: ['OpenAI', 'GPT', 'BERT', 'TensorFlow']
+    }
+};
+
+export default tecnologias
